b3c26173a35c2a7338a5fe5c10557b5464ee5d06
RlonRyan <rlonryan@aol.com>
2015-11-25 01:28:13 -0700

    Add Boolean Converter
    Debating the way capitalization is handled.

--- src/main/modcmd/converters/StaticConverters.java
+++ src/main/modcmd/converters/StaticConverters.java
@@ -8,12 +8,30 @@ package modcmd.converters;
 import modcmd.commands.CommandManager;
 import modcmd.commands.CommandNode;
 import modcmd.converters.exceptions.ConversionException;
+import modcmd.converters.exceptions.ConverterException;
 
 /**
  *
  * @author RlonRyan
  */
 public class StaticConverters {
+    
+    @Converter({"boolean", "bool", "b"})
+    public static boolean getBool(Object user, String tag, String value) throws ConverterException {
+
+        switch (value.charAt(0)) {
+            case 'F':
+            case 'f':
+            case '0':
+                return false;
+            case 'T':
+            case 't':
+            case '1':
+                return true;
+        }
+
+        throw new ConversionException(tag, value, "Boolean");
+    }
 
     @Converter({"integer", "int", "i"})
     public static int convertInteger(Object user, String tag, String parameter) throws ConversionException {
